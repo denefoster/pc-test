ca1842a097917ec15671247ffa4d88991833db9f
dane foster <dene@slush.ca>
2026-05-22 12:38:00 +1200

    .

--- postconfirm.py
+++ postconfirm.py
@@ -25,13 +25,12 @@ async def main():
     app_config = config.Config(args.config_file)
 
     # Set up the root logger
-    logging.basicConfig(level=app_config.get('log.level', logging.WARNING))
     logging.basicConfig(
-    level=app_config.get('log.level', logging.WARNING),
-    style="{",
-    datefmt="%b %d %H:%M:%S",
-    format="{asctime} postconfirm/postconfirm[{process}]: {message} [{filename}:{lineno}]"
-)
+        level=app_config.get('log.level', logging.INFO),
+        style="{",
+        datefmt="%b %d %H:%M:%S",
+        format="{asctime} postconfirm/postconfirm[{process}]: {message} [{filename}:{lineno}]"
+    )
 
 
     # Set up a services registry
