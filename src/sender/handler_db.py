fec5a9fa98c8019cd3545de8da90e949caedb223
dane foster <dene@slush.ca>
2026-09-19 12:14:01 +1200

    .

--- src/sender/handler_db.py
+++ src/sender/handler_db.py
@@ -37,7 +37,7 @@ class HandlerDb:
                         FROM senders
                         WHERE sender=%(sender)s AND type='E'
                     """,
-                    {"sender": sender}
+                    {"sender": sender.lower()}
                 )
                 result = cursor.fetchone()
 
@@ -48,7 +48,7 @@ class HandlerDb:
                         FROM senders_static
                         WHERE sender=%(sender)s AND type='E'
                     """,
-                    {"sender": sender}
+                    {"sender": sender.lower()}
                 )
                 static_result = cursor.fetchone()
 
@@ -120,7 +120,7 @@ class HandlerDb:
             with connection.cursor() as cursor:
                 cursor.execute(
                     "SELECT 1 FROM never_allow WHERE email = %(sender)s",
-                    {"sender": sender}
+                    {"sender": sender.lower()}
                 )
                 return cursor.fetchone() is not None
 
@@ -142,7 +142,7 @@ class HandlerDb:
                             ON CONFLICT (sender)
                                 DO UPDATE SET action=%(action)s, updated=now()
                         """,
-                        {"sender": sender, "action": action, "ref": parsed_ref}
+                        {"sender": sender.lower(), "action": action, "ref": parsed_ref}
                     )
                     connection.commit()
                     return True
@@ -167,7 +167,7 @@ class HandlerDb:
                             VALUES
                                 (%(sender)s, %(recipients)s, %(message)s)
                         """,
-                        {"sender": sender, "recipients": json.dumps(recipients), "message": msg}
+                        {"sender": sender.lower(), "recipients": json.dumps(recipients), "message": msg}
                     )
                     connection.commit()
                     return True
@@ -192,7 +192,7 @@ class HandlerDb:
                             FROM stash
                             WHERE sender=%(sender)s
                         """,
-                        {"sender": sender}
+                        {"sender": sender.lower()}
                     )
 
                     for (row_id, recipients, message) in cursor:
@@ -215,7 +215,7 @@ class HandlerDb:
                             FROM stash_static
                             WHERE sender=%(sender)s
                         """,
-                        {"sender": sender}
+                        {"sender": sender.lower()}
                     )
 
                     for (row_id, recipients, message) in cursor:
